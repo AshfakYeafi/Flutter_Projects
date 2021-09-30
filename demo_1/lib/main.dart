4b67d89277561f362435c8d9ca7b2de52615dae2
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-10-01 03:03:50 +0600

    update main.dart

--- demo_1/lib/main.dart
+++ demo_1/lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return MyAppState();
   }
- 
-
 }
 
-class MyAppState extends State<MyApp>{
-
-
+class MyAppState extends State<MyApp> {
   var count = 0;
 
   void answerQue() {
@@ -28,8 +22,14 @@ class MyAppState extends State<MyApp>{
 
   @override
   Widget build(BuildContext context) {
-    var question = ["What is your name?", "How are you?","What is your name?", "How are you?","What is your name?", "How are you?"];
-
+    var que = [
+      "What is your name?",
+      "Where do you live?",
+      "Will you be my gf?",
+      "Will you be my gf_2?",
+      "Will you be my gf_3?"
+    ];
+    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -37,14 +37,13 @@ class MyAppState extends State<MyApp>{
         ),
         body: Column(
           children: [
-            Text(question[count]),
-            RaisedButton(child: Text("This is button 1"), onPressed: answerQue),
-            RaisedButton(child: Text("This is button 2"), onPressed: answerQue),
-            RaisedButton(child: Text("This is button 3"), onPressed: answerQue),
+            Text(que[count]),
+            RaisedButton(child: Text("Button 1"), onPressed: answerQue),
+            RaisedButton(child: Text("Button 2"), onPressed: answerQue),
+            RaisedButton(child: Text("Button 3"), onPressed: answerQue),
           ],
         ),
       ),
     );
   }
 }
-
