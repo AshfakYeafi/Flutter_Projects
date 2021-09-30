9bff862b658dc0be1301b820875487bd7368bdea
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-10-01 01:47:25 +0600

    Update main.dart

--- demo_1/lib/main.dart
+++ demo_1/lib/main.dart
@@ -6,17 +6,25 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void answerQue() {
+    print("I fuck You");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("This is tittle"),),
-        body:Column(children: [
-          RaisedButton(child: Text("This is button 1"), onPressed: null),
-          RaisedButton(child: Text("This is button 2"), onPressed: null),
-          RaisedButton(child: Text("This is button 3"), onPressed: null),
-        ],) ,
+        appBar: AppBar(
+          title: Text("This is tittle"),
         ),
+        body: Column(
+          children: [
+            RaisedButton(child: Text("This is button 1"), onPressed: answerQue),
+            RaisedButton(child: Text("This is button 2"), onPressed: answerQue),
+            RaisedButton(child: Text("This is button 3"), onPressed: answerQue),
+          ],
+        ),
+      ),
     );
   }
 }
